a3dd46346e93f42a3ff67ed429905597757e7581
Sharoar Hossain <maffin.killer007@gmail.com>
2026-02-09 13:55:56 +0600

    The Mechanism With Load Video & Display Video Card

--- scripts/index.js
+++ scripts/index.js
@@ -22,4 +22,45 @@ const displayCategories = categories => {
     });
 }
 
+// load videos
+const loadVideos = async () => {
+    try {
+        const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos');
+        const data = await res.json();
+        const videos = data.videos;
+        displayVideos(videos);
+    } catch (error) {
+        console.error('Error fetching videos: ', error);
+    }
+}
+
+// display videos
+const displayVideos = videos => {
+    const videoContainer = document.getElementById('videoContainer');
+
+    videos.forEach(video => {
+        const videoDiv = document.createElement('div');
+        videoDiv.innerHTML = `
+            <div class="card bg-base-100 shadow-sm">
+                <figure>
+                    <img
+                    class="w-full"
+                    src=${video.thumbnail}
+                    alt=${video.title || 'Image'} />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">${video.title}</h2>
+                    <p>${video.description}</p>
+                    <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+                </div>
+        `;
+        videoContainer.appendChild(videoDiv);
+    })
+}
+
+
+loadVideos();
 loadCategoties();
